c611f6c699d0a157474d027d86dbba0deffa1e8a
Faezeh <faezehadabi9@gmail.com>
2025-12-15 19:47:52 +0330

    Show wishlist

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,27 +1,35 @@
 import { useState } from "react";
-
 import { books } from "../constants/mockData";
 import BookCard from "./BookCard";
+import SideCard from "./sideCard";
+import styles from "./Books.module.css";
 
 function Books() {
   const [Liked, setLiked] = useState([]);
   const handleLikeList = (book, status) => {
-    if(status){
-      const NewLikeList = Liked.filter((i)=>i.id !== book.id);
+    if (status) {
+      const NewLikeList = Liked.filter((i) => i.id !== book.id);
       setLiked(NewLikeList);
-    }else{
-      setLiked((Liked) => [...Liked , book]);
+    } else {
+      setLiked((Liked) => [...Liked, book]);
     }
   };
 
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.cards}>
         {books.map((book) => (
           <BookCard key={book.id} data={book} handleLikeList={handleLikeList} />
         ))}
       </div>
-      {!!Liked.length && <div>ww</div>  }
+      {!!Liked.length && (
+        <div className={styles.favorite}>
+          <h4>Favorites</h4>
+          {Liked.map((book) => (
+            <SideCard key={book.id} data={book} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
